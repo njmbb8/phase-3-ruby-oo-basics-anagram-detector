a977540f8939971c8206feb5c7620d2056d7c9a7
njmbb8 <nmcgr001@gmail.com>
2021-10-07 17:20:36 -0400

    commit tree 43b62a463418156e088417d058abb6a5c3bffc52

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,9 @@
-# Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words)
+        words.filter { |comp| comp.chars.sort == @word.chars.sort }
+    end
+end
